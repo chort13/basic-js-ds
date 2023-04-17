55718b7ae623f303dc5a2b29d5f973a6445c22d8
Tim_Pans_13 <95716918+chort13@users.noreply.github.com>
2023-04-18 00:30:09 +0300

    Update queue.js

--- src/queue.js
+++ src/queue.js
@@ -1,35 +1,70 @@
-const { NotImplementedError } = require('../extensions/index.js');
+//const { NotImplementedError } = require('../extensions/index.js');
 
-// const { ListNode } = require('../extensions/list-node.js');
+ const { ListNode } = require('../extensions/list-node.js');
 
-/**
- * Implement the Queue with a given interface via linked list (use ListNode extension above).
- *
- * @example
- * const queue = new Queue();
- *
- * queue.enqueue(1); // adds the element to the queue
- * queue.enqueue(3); // adds the element to the queue
- * queue.dequeue(); // returns the top element from queue and deletes it, returns 1
- * queue.getUnderlyingList() // returns { value: 3, next: null }
- */
+ class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
 class Queue {
-
-  getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.size = 0;
   }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  enqueue(element) {
+    const newNode = new Node(element);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.size++;
   }
 
   dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (!this.head) {
+      return;
+    }
+    const removedNode = this.head;
+    if (this.head === this.tail) {
+      this.tail = null;
+    }
+    this.head = this.head.next;
+    this.size--;
+    return removedNode.value;
+  }
+
+ 
+
+  getUnderlyingList() 
+  {
+    let str = '';
+    let current = this.head;
+    while (current) {
+      str += `{"value":${current.value},"next":`;
+      if (current.next) {
+        str += '{';
+      } else {
+        str += 'null';
+      }
+      current = current.next;
+    }
+    str += '}'.repeat(this.length - 1);
+   
+    str = str.replace(/\\/g, '');
+
+    return str;
   }
 }
+
+
 
 module.exports = {
   Queue
